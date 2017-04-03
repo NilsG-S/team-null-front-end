bb0375411b6e1fb1990d6f7e13c59a1689ada2ec
NilsG-S <NilsG-S@users.noreply.github.com>
2017-04-03 12:46:21 -0500

    Added linter settings

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "extends": "airbnb",
+  "env": {
+    "mocha": true,
+    "node": true,
+    "browser": true,
+  },
+  "plugins": ["chai-friendly"],
+  "rules": {
+    "chai-friendly/no-unused-expressions": 2,
+    "import/no-extraneous-dependencies": 0,
+    "import/extensions": 0,
+    "import/no-unresolved": 0,
+  }
+};
