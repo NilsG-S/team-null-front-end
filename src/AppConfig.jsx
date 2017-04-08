cf918595732b8d782a43321df59cb23ccd07f413
NilsG-S <NilsG-S@users.noreply.github.com>
2017-04-08 16:30:48 -0500

    Switched to react-router v4

--- src/AppConfig.jsx
+++ src/AppConfig.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Router, hashHistory } from 'react-router';
+import { HashRouter, Route, Redirect } from 'react-router-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
+import App from 'grommet/components/App';
+import Box from 'grommet/components/Box';
+
 import healthApp from 'redux/reducers.js';
-import appRoute from './app-route.jsx';
+import authRoute from './routes/Auth';
+import calendarRoute from './routes/Calendar';
 
 // store holds the redux store that allows app-wide state to be shared
 const store = createStore(healthApp);
@@ -12,7 +16,20 @@ const store = createStore(healthApp);
 const AppConfig = () => (
   // Provider shares store with components joined by connect()
   <Provider store={store}>
-    <Router history={hashHistory} routes={appRoute} />
+    <HashRouter>
+      <App
+        centered={false}
+        inline={false}
+      >
+        <Box full>
+          <Route exact path="/" render={() => (
+            <Redirect to="/auth"/>
+          )}/>
+          {authRoute}
+          {calendarRoute}
+        </Box>
+      </App>
+    </HashRouter>
   </Provider>
 );
 
