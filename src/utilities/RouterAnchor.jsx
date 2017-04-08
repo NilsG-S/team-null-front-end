a68e232752d0459a2d91f159b477788b7d43a996
NilsG-S <NilsG-S@users.noreply.github.com>
2017-04-08 18:02:43 -0500

    Fixed problem with RouterAnchor

--- src/utilities/RouterAnchor.jsx
+++ src/utilities/RouterAnchor.jsx
@@ -4,7 +4,20 @@ import { withRouter } from 'react-router-dom';
 import Anchor from 'grommet/components/Anchor';
 
 function RouterAnchor({ history, path, ...rest }) {
-  return (<Anchor {...rest} onClick={() => history.push(path)} />);
+  const properties = rest;
+  let clickFunction = null;
+
+  if (path === rest.location.pathname) {
+    clickFunction = () => {};
+  } else {
+    clickFunction = () => history.push(path);
+  }
+
+  delete properties.match;
+  delete properties.location;
+  delete properties.staticContext;
+
+  return (<Anchor {...properties} onClick={clickFunction} />);
 }
 
 RouterAnchor.propTypes = {
