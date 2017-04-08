8a9c17cd4564a293db3bf74de6ed38e95889875c
NilsG-S <NilsG-S@users.noreply.github.com>
2017-04-08 11:51:25 -0500

    Using requireAuth prototype

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,14 +3,25 @@ import React from 'react';
 import Box from 'grommet/components/Box';
 
 import NavBarContainer from 'components/NavBar/NavBarContainer.jsx';
+import requireAuth from '../RequireAuth.js';
 
-function Main(props) {
-  return (
-    <Box>
-      <NavBarContainer />
-      {props.children}
-    </Box>
-  );
+class Main extends React.Component {
+  render() {
+    return (
+      <Box>
+        <NavBarContainer />
+        {this.props.children}
+      </Box>
+    );
+  }
 }
 
-export default Main;
+const requiredTypes = {
+  0: false,
+  1: true,
+  2: true,
+  3: true,
+  4: true,
+};
+
+export default requireAuth(Main, requiredTypes);
