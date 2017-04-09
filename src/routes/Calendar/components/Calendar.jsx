32bf1915173cbee8a06b63c4eea1fd09bdfb060b
NilsG-S <NilsG-S@users.noreply.github.com>
2017-04-09 12:41:16 -0500

    Added resizing of calendar to maintain aspect ratio

--- src/routes/Calendar/components/Calendar.jsx
+++ src/routes/Calendar/components/Calendar.jsx
@@ -2,15 +2,76 @@ import React from 'react';
 
 import Box from 'grommet/components/Box';
 
-import NavBarContainer from 'components/NavBar/NavBarContainer.jsx';
 import protectRoute from 'utilities/ProtectRoute.jsx';
 
 class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      size: 500
+    };
+
+    this.resizeCalendar = this.resizeCalendar.bind(this);
+  }
+
+  resizeCalendar() {
+    const element = document.getElementById(
+      'routes-calendar-components-calendar-div-1'
+    );
+    const newHeight = element.parentElement.clientHeight;
+    const newWidth = element.parentElement.clientWidth;
+    let newSize;
+
+    if (newWidth > newHeight) {
+      newSize = newHeight;
+    } else {
+      newSize = newWidth;
+    }
+
+    this.setState({
+      size: newSize
+    });
+  }
+
+  componentDidMount() {
+    this.resizeCalendar();
+    window.addEventListener('resize', this.resizeCalendar);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.resizeCalendar);
+  }
+
   render() {
+    let main = {
+      height: '100%',
+      width: '100%',
+    };
+
+    let style = {
+      height: this.state.size,
+      width: this.state.size,
+      display: 'flex',
+      flexDirection: 'column',
+    };
+
+    let style2 = {
+      flex: 1,
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+    };
+
     return (
-      <Box>
-        <NavBarContainer />
-        <h2>Calendar</h2>
+      <Box flex align='center' justify='center'>
+        <div id='routes-calendar-components-calendar-div-1' style={style}>
+          <div style={style2}>
+            <h2>Calendar</h2>
+          </div>
+          <div style={style2}>
+            <h2>Calendar</h2>
+          </div>
+        </div>
       </Box>
     );
   }
