b77d6ffa16d5aa5f3e793dfa6cbfd81f0637c059
NilsG-S <NilsG-S@users.noreply.github.com>
2017-04-18 05:02:00 -0500

    Schedule redirects to record for doctors

--- src/routes/Calendar/components/Schedule.jsx
+++ src/routes/Calendar/components/Schedule.jsx
@@ -7,7 +7,7 @@ import List from 'grommet/components/List';
 import ListItem from 'grommet/components/ListItem';
 
 import { dateToKey } from 'server/appointments.js';
-import { setDate } from 'redux/actions.js';
+import { setDate, AuthStates } from 'redux/actions.js';
 
 class Schedule extends React.Component {
   constructor(props) {
@@ -35,7 +35,12 @@ class Schedule extends React.Component {
     // this.props.dispatch(setDate({
     //   hour: this.props.date.getDate(),
     // }));
-    this.props.history.push('/appointment');
+
+    if (this.props.type === AuthStates.STAFF) {
+      this.props.history.push('/appointment');
+    } else {
+      this.props.history.push('/record');
+    }
   }
 
   handleClose() {
@@ -116,12 +121,14 @@ Schedule.propTypes = {
   appointments: React.PropTypes.shape({
     has: React.PropTypes.func.isRequired,
   }).isRequired,
+  type: React.PropTypes.number.isRequired,
 };
 
 function mapStateToProps(state) {
   return {
     date: state.date,
     appointments: state.appointments,
+    type: state.user.type,
   };
 }
 
