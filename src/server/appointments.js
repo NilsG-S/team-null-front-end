a8becbf79323f4c8721494da3e96154f710009dd
NilsG-S <NilsG-S@users.noreply.github.com>
2017-05-03 08:39:19 -0500

    Added getUncompAppsByDoctor

--- src/server/appointments.js
+++ src/server/appointments.js
@@ -66,3 +66,43 @@ export function getUncompApps(month) {
     xhr.send();
   });
 }
+
+export function getUncompAppsByDoctor(uid, month) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `${url}appointments/doctor/uncompleted/${uid}/${month}`);
+    xhr.onload = () => {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        const map = new Map();
+        let date;
+
+        JSON.parse(xhr.response).forEach((element) => {
+          date = new Date(element.date_time);
+
+          map.set(dateToKey(date), {
+            id: element.id,
+            employee_id: element.employee_id,
+            patient_id: element.patient_id,
+            date_time: date,
+            completed: element.completed,
+          });
+        });
+
+        store.dispatch(cacheAppointments(map));
+        resolve(map);
+      } else {
+        reject({
+          status: xhr.status,
+          statusText: xhr.statusText,
+        });
+      }
+    };
+    xhr.onerror = () => {
+      reject({
+        status: xhr.status,
+        statusText: xhr.statusText,
+      });
+    };
+    xhr.send();
+  });
+}
