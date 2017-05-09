7ca9549f8238bf1e7c45a28e21b29bbdb4ff95ea
NilsG-S <NilsG-S@users.noreply.github.com>
2017-05-09 02:43:18 -0500

    Implemented auth API, removed useless code

--- src/server/auth.js
+++ src/server/auth.js
@@ -1,63 +1,42 @@
-import { updateUser, logoutUser, AuthStates } from 'redux/actions.js';
+import { updateUser, logoutUser } from 'redux/actions.js';
 import { store } from 'AppConfig.jsx';
-
-const employees = {
-  1: {
-    id: 1,
-    first_name: 'CEO',
-    last_name: 'Test',
-    type: AuthStates.CEO,
-    associated_id: null,
-  },
-  2: {
-    id: 2,
-    first_name: 'Doctor',
-    last_name: 'Test',
-    type: AuthStates.DOCTOR,
-    associated_id: 3,
-  },
-  3: {
-    id: 3,
-    first_name: 'Nurse',
-    last_name: 'Test',
-    type: AuthStates.NURSE,
-    associated_id: 2,
-  },
-  4: {
-    id: 4,
-    first_name: 'Staff',
-    last_name: 'Test',
-    type: AuthStates.STAFF,
-    associated_id: null,
-  },
-};
-
-export function signIn(id, password) {
-  return new Promise((resolve, reject) => {
-    if (id in employees) {
-      const user = employees[id];
-      store.dispatch(updateUser(user));
-      resolve(user);
-    } else {
-      reject(Error('No such employee'));
-    }
-  });
-}
-
-export function register(user, password) {
-  return new Promise((resolve, reject) => {
-    if (!(user.id in employees)) {
-      store.dispatch(updateUser(user));
-      resolve(user);
-    } else {
-      reject(Error('Cannot create employee'));
-    }
-  });
-}
+import { url } from './utils.js';
 
 export function signOut() {
   return new Promise((resolve, reject) => {
     store.dispatch(logoutUser());
-    resolve(undefined);
+    resolve();
+  });
+}
+
+export function login(id, password) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', `${url}auth`);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = () => {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        const user = JSON.parse(xhr.response);
+
+        store.dispatch(updateUser(user));
+
+        resolve(user);
+      } else {
+        reject({
+          status: xhr.status,
+          statusText: xhr.statusText,
+        });
+      }
+    };
+    xhr.onerror = () => {
+      reject({
+        status: xhr.status,
+        statusText: xhr.statusText,
+      });
+    };
+    xhr.send(JSON.stringify({
+      id,
+      password,
+    }));
   });
 }
